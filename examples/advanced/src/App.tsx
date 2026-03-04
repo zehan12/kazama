fcd7f224671f71b071398658f8dd1a93f084df48
zehan12 <zehan9211@gmail.com>
2026-03-04 09:45:00 +0530

    refactor: ♻️ modularize components

---
+++ examples/advanced/src/App.tsx
@@ -0,0 +1,18 @@
+import { BackgroundJobDashboard } from './components/BackgroundJobDashboard';
+import { DirectApiRequest } from './components/DirectApiRequest';
+import { ComplexWorkflowAuth } from './components/ComplexWorkflowAuth';
+import { LargeSharedState } from './components/LargeSharedState';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', maxWidth: 800, margin: '0 auto', padding: 20 }}>
+      <h1>@react-store/core - Architecture Showcase</h1>
+      <DirectApiRequest />
+      <BackgroundJobDashboard />
+      <ComplexWorkflowAuth />
+      <LargeSharedState />
+    </div>
+  );
+}
+
+export default App;
