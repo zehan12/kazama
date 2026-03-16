7af9de00c56a1bce1b068f6b3063c6c937078cc6
zehan12 <zehan9211@gmail.com>
2026-03-16 19:49:00 +0530

    feat: ✨ add request api mock

---
+++ examples/blog/src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore, createRequestInstance, setRequestInstance } from '@react-store/core';
+import * as models from '../models';
+
+const requestClient = createRequestInstance()['default'];
+setRequestInstance({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+}, requestClient);
+
+export const store = createStore(models, {
+  name: 'BlogStore',
+  request: requestClient,
+});
+
+export const { useModel, useModelState, useModelDispatchers, useModelEffectsLoading, useRequest } = store;
