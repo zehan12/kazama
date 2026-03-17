65cbc7369af1ad5fe40fc441baf6c502c094f4a8
zehan12 <zehan9211@gmail.com>
2026-03-17 18:57:00 +0530

    test: ✅ prepare testing environment

---
+++ examples/request/src/store/index.ts
@@ -0,0 +1,21 @@
+import { createRequestInstance, setRequestInstance } from '@react-store/core';
+
+createRequestInstance();
+
+setRequestInstance({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  interceptors: {
+    request: {
+      onConfig: (config: any) => {
+        config.headers = { ...config.headers, 'X-Example-Header': 'RequestExample' };
+        return config;
+      }
+    }
+  }
+});
+
+// Since we only need the request functionality for this example,
+// we export a mock store that only exposes `useRequest`.
+import { createStore } from '@react-store/core';
+export const store = createStore({});
+export const { useRequest } = store;
