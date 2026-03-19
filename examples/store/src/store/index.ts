b65ab4a8fb747f46bec9f988be0e9d8bf7541543
zehan12 <zehan9211@gmail.com>
2026-03-19 10:24:00 +0530

    chore: 🔧 configure typescript

---
+++ examples/store/src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore } from '@react-store/core';
+import * as models from '../models';
+
+export const store = createStore(models, {
+  name: 'StoreExample',
+  persist: { key: 'store_example_state', storage: 'localStorage' },
+});
+
+export const { useModel, useModelState, useModelDispatchers } = store;
