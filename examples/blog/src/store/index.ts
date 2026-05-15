613f20336de13de98cb1cb8e23bcdaa9080efebc
zehan12 <zehan9211@gmail.com>
2026-05-15 17:01:00 +0530

    refactor: ♻️ update musubi imports in blog example

--- examples/blog/src/store/index.ts
+++ examples/blog/src/store/index.ts
@@ -1,4 +1,4 @@
-import { createStore, createRequestInstance, setRequestInstance } from '@musubi/core';
+import { createStore, createRequestInstance, setRequestInstance } from 'musubi';
 import * as models from '../models';
 
 const requestClient = createRequestInstance()['default'];
