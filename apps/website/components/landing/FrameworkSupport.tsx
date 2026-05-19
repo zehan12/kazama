10a2dcccbb9442313af5f81e98b0754b9f850823
zehan12 <zehan9211@gmail.com>
2026-05-19 17:31:00 +0530

    refactor: ♻️ update framework support UI in landing

--- apps/website/components/landing/FrameworkSupport.tsx
+++ apps/website/components/landing/FrameworkSupport.tsx
@@ -4,8 +4,6 @@ import { useTheme } from 'next-themes'
 import { Activity } from 'react'
 
 export default function FrameworkSupport() {
-
-    const { resolvedTheme } = useTheme()
     return (
         <section>
             <div className="mt-12 mb-8">
@@ -13,16 +11,12 @@ export default function FrameworkSupport() {
                 <div className="flex items-center gap-6 duration-300">
                     <React className="w-8 h-8" />
                     <NextJs className="w-8 h-8" />
-                    <Activity mode={resolvedTheme === "dark" ? "visible" : "hidden"}>
-                        <RemixLight className="w-6 h-6 " />
-                    </Activity>
-                    <Activity mode={resolvedTheme === "dark" ? "hidden" : "visible"}>
-                        <RemixDark className="w-6 h-6 " />
-                    </Activity>
-                    <img src="vite.svg" alt="vite" className="w-6 h-6" />
+                    <RemixLight className="w-6 h-6 hidden dark:block" />
+                    <RemixDark className="w-6 h-6 block dark:hidden" />
+                    <img src="/vite.svg" alt="vite" className="w-6 h-6" />
                     <Preact className="w-8 h-8" />
                     <img src="https://nuqs.dev/tanstack-logo.png" alt="TanStack" className="w-8 h-8 object-contain" />
-                    <ReactRouter className="w-8 h-8" />
+                    <ReactRouter className="w-7 h-7" />
                 </div>
             </div>
         </section>
