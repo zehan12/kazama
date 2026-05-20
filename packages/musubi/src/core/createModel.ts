a2016b5f7df93d5d4213e44765c1d4f303574e4b
zehan12 <zehan9211@gmail.com>
2026-05-20 19:40:00 +0530

    chore: 🧹 remove old musubi package files

--- packages/musubi/src/core/createModel.ts
+++
@@ -1,5 +0,0 @@
-import { Model } from '../types';
-
-export function createModel<State, Reducers, Effects>(model: Model<State, Reducers, Effects>) {
-  return model;
-}
--- packages/musubi/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { createStore } from './core/createStore';
-export { createModel } from './core/createModel';
-export * from './network/request';
-export * from './react/loader';
-export * from './react/parsers';
-export * from './react/url-state';
-export * from './types';
