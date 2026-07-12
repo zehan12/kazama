b6f2d8b73bfbb19e4706ddff37701ed82517a869
zehan12 <zehan9211@gmail.com>
2026-07-12 18:25:00 +0530

    fix: 🐛 update transpilePackages in next config to use scoped kazama package

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['kazama'],
+  transpilePackages: ['@zehankhan/kazama'],
   typescript: { ignoreBuildErrors: true }
 }
 
